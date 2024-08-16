ccc48456dc52f08a205fac54d4ac744ab1ebcfdd
Cs-kh <khalidrahim487@gmail.com>
2024-08-17 02:24:18 +0300

    the first page is done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import FirstPage from "./components/FirstPage";
+
+function App() {
+  return (
+    <main>
+      <FirstPage />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/FirstPage.jsx
@@ -0,0 +1,23 @@
+import "./FirstPage.css";
+
+export default function FirstPage() {
+  return (
+    <main>
+      <div className="first-page">
+        <div className="">
+          <h1>
+            <span className="span-1 span">t</span>
+            <span className="span-2 span">o</span>
+            <span className="span-3 span">d</span>
+            <span className="span-4 span">o</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+            minima?Lorem ipsum dolor sit amet lorem
+          </p>
+          <button>get started</button>
+        </div>
+      </div>
+    </main>
+  );
+}
